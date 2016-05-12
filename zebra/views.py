3ac1a1cf31084d9b48feb9f7550b11e1d998fb91
Ben Lopatin <ben@benlopatin.com>
2016-05-12 15:34:13 -0400

    Replace models.get_model and fix overzealous renaming

--- zebra/views.py
+++ zebra/views.py
@@ -2,21 +2,21 @@ import json
 import logging
 
 import stripe
-from django.db.models import get_model
+from django.apps import apps
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from zebra.conf import options
 from zebra.signals import *
 
-log = logging.getLogger("zebra.%s" % __name__)
+log = logging.getLogger("zebra.{0}".format(__name__))
 
 stripe.api_key = options.STRIPE_SECRET
 
 
 def _try_to_get_customer_from_customer_id(stripe_customer_id):
     if options.ZEBRA_CUSTOMER_MODEL:
-        m = get_model(*options.ZEBRA_CUSTOMER_MODEL.split('.'))
+        m = apps.get_model(*options.ZEBRA_CUSTOMER_MODEL.split('.'))
         try:
             return m.objects.get(stripe_customer_id=stripe_customer_id)
         except:
@@ -34,39 +34,39 @@ def webhooks(request):
     if request.method != "POST":
         return HttpResponse("Invalid Request.", status=400)
 
-    json = json.loads(request.POST["json"])
+    request_data = json.loads(request.POST["request_data"])
 
-    if json["event"] == "recurring_payment_failed":
+    if request_data["event"] == "recurring_payment_failed":
         zebra_webhook_recurring_payment_failed.send(
             sender=None,
-            customer=_try_to_get_customer_from_customer_id(json["customer"]),
-            full_json=json)
+            customer=_try_to_get_customer_from_customer_id(request_data["customer"]),
+            full_request_data=request_data)
 
-    elif json["event"] == "invoice_ready":
+    elif request_data["event"] == "invoice_ready":
         zebra_webhook_invoice_ready.send(
             sender=None,
-            customer=_try_to_get_customer_from_customer_id(json["customer"]),
-            full_json=json)
+            customer=_try_to_get_customer_from_customer_id(request_data["customer"]),
+            full_request_data=request_data)
 
-    elif json["event"] == "recurring_payment_succeeded":
+    elif request_data["event"] == "recurring_payment_succeeded":
         zebra_webhook_recurring_payment_succeeded.send(
             sender=None,
-            customer=_try_to_get_customer_from_customer_id(json["customer"]),
-            full_json=json)
+            customer=_try_to_get_customer_from_customer_id(request_data["customer"]),
+            full_request_data=request_data)
 
-    elif json["event"] == "subscription_trial_ending":
+    elif request_data["event"] == "subscription_trial_ending":
         zebra_webhook_subscription_trial_ending.send(
             sender=None,
-            customer=_try_to_get_customer_from_customer_id(json["customer"]),
-            full_json=json)
+            customer=_try_to_get_customer_from_customer_id(request_data["customer"]),
+            full_request_data=request_data)
 
-    elif json["event"] == "subscription_final_payment_attempt_failed":
+    elif request_data["event"] == "subscription_final_payment_attempt_failed":
         zebra_webhook_subscription_final_payment_attempt_failed.send(
             sender=None,
-            customer=_try_to_get_customer_from_customer_id(json["customer"]),
-            full_json=json)
+            customer=_try_to_get_customer_from_customer_id(request_data["customer"]),
+            full_request_data=request_data)
 
-    elif json["event"] == "ping":
+    elif request_data["event"] == "ping":
         zebra_webhook_subscription_ping_sent.send(sender=None)
 
     else:
@@ -84,12 +84,7 @@ def webhooks_v2(request):
     if request.method != "POST":
         return HttpResponse("Invalid Request.", status=400)
 
-    try:
-        event_json = json.loads(request.body)
-    except AttributeError:
-        # Backwords compatibility
-        # Prior to Django 1.4, request.body was named request.raw_post_data
-        event_json = json.loads(request.raw_post_data)
+    event_json = json.loads(request.body)
     event_key = event_json['type'].replace('.', '_')
 
     if event_key in WEBHOOK_MAP:
